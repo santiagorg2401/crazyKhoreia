81d480c69ec67a1bc3a1e2696f5d8902c81f5043
Santiago Restrepo <sanrega24@gmail.com>
2022-04-11 21:42:35 -0500

    Unused dependencies removed, code organized, path optimization for lightPainting using mlrose deleted, logic for sleepTime changed, now it is a percentage, clean_waypoints method changed from crazyKhoreia into lightPainting and aprozimation method in findContours changed.

--- src/crazyKhoreia/lightPainting.py
+++ src/crazyKhoreia/lightPainting.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
-import itertools
 import datetime
-import sys
-import six
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from crazyKhoreia.crazyKhoreia import crazyKhoreia
 
-sys.modules['sklearn.externals.six'] = six
-import mlrose
 
 class lightPainting():
-    def __init__(self, MAX_WIDTH, MAX_HEIGHT, MIN_WIDTH, MIN_HEIGHT, in_path, out_path, detail=0.05, speed=1.0, sleepTime=0.1, video=False, led=False):
+    def __init__(self, MAX_WIDTH, MAX_HEIGHT, MIN_WIDTH, MIN_HEIGHT, in_path, out_path, detail=0.05, speed=1.0, sleepTime=1.5, video=False, led=False):
 
         self.MAX_WIDTH, self.MAX_HEIGHT, self.MIN_WIDTH, self.MIN_HEIGHT, self.in_path, self.out_path = MAX_WIDTH, MAX_HEIGHT, MIN_WIDTH, MIN_HEIGHT, in_path, out_path
         self.detail, self.speed, self.sleepTime, self.video, self.led = detail, speed, sleepTime, video, led
@@ -33,64 +28,16 @@ class lightPainting():
 
         plt.show()
 
-    def get_distances(self, cnt_scaled, ImgShape_X, ImgShape_Y):
-        strPoints = np.empty((0, 2))
-        endPoints = np.empty((0, 2))
-        distances = np.array([])
-
-        # Iterate contours.
-        for i in range(0, len(self.cnt_scaled)):
-            x = np.array(self.cnt_scaled[i])
-            x = np.reshape(x, (len(self.cnt_scaled[i]), 2))
-
-            strPoints = np.vstack([strPoints, [x[0, 0], x[0, 1]]])
-            endPoints = np.vstack([endPoints, [x[-1, 0], x[-1, 1]]])
-
-        combs = np.array(
-            list(itertools.combinations(range(0, len(cnt_scaled)), 2)))
-
-        for i in combs:
-            start_ = i[0]
-            end_ = i[1]
-
-            dist = np.linalg.norm(endPoints[start_] - strPoints[end_])
-            distances = np.append(distances, dist)
-
-        dist_list = np.array([combs[:, 0], combs[:, 1], distances]).T
-
-        return dist_list
-
-    def find_order(self, cnt_scaled, dist_list):
-        # Initialize fitness function object using dist_list
-        fitness_dists = mlrose.TravellingSales(distances=dist_list)
-
-        # Define optimization problem object
-        problem_fit = mlrose.TSPOpt(length=len(
-            cnt_scaled), fitness_fn=fitness_dists, maximize=False)
-
-        # Solve using genetic algorithm
-        best_state, best_fitness, fitness_curve = mlrose.genetic_alg(
-            problem_fit, mutation_prob=0.2, max_attempts=100, random_state=2, curve=True)
-
-        #best_state, best_fitness = mlrose.random_hill_climb(problem_fit, max_attempts=100, random_state=2)
-
-        fig, ax = plt.subplots()
-        ax.plot(fitness_curve, label='Fitness curve.')
-        ax.legend()
-        ax.set_title('Fitness curve.')
-
-        return best_state
-
     def clean_waypoints(self, wayPoints, detail):
         # Clean wayPoints by a detail parameter.
         initialPoints = len(wayPoints)
 
         for j in range(0, 10):
-          for i in range(1, initialPoints):
-              if ((i + 1 == len(wayPoints)) | (i == len(wayPoints))):
-                  break
-              elif (np.linalg.norm(wayPoints[:, 0:3][i - 1] - wayPoints[:, 0:3][i]) < detail):
-                  wayPoints = np.delete(wayPoints, i - 1, 0)
+            for i in range(1, initialPoints):
+                if ((i + 1 == len(wayPoints)) | (i == len(wayPoints))):
+                    break
+                elif (np.linalg.norm(wayPoints[:, 0:3][i - 1] - wayPoints[:, 0:3][i]) < detail) & (wayPoints[:, 3][i] == 1):
+                    wayPoints = np.delete(wayPoints, i, 0)
 
         return wayPoints
 
@@ -111,7 +58,8 @@ class lightPainting():
             else:
                 distance += abs(np.linalg.norm(
                     self.wayPoints[:, 0:3][i - 1] - self.wayPoints[:, 0:3][i]))
-        Time = distance/self.speed + len(self.wayPoints)*self.sleepTime
+
+        Time = distance/self.speed*self.sleepTime
 
         return distance, Time
 
